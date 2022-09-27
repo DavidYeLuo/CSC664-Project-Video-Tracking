829db2da30619264794ae55a19e4df8cfc244925
Student <davidayeluo@gmail.com>
2022-09-27 13:15:36 -0700

    Fixed issues with trying to use opencv.

--- src/ImageReader/temp/A.java
+++ src/ImageReader/temp/A.java
@@ -1,9 +1,21 @@
+// Temporary class.
 package temp;
+
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
 
 public class A
 {
     public void print()
     {
-        System.out.println("It's workign");
+        System.out.println("------------------------------------");
+        System.out.println("Welcome to OpenCV " + Core.VERSION);
+        System.out.println("Java library path: " + System.getProperty("java.library.path"));
+        System.out.println("Core: " + Core.NATIVE_LIBRARY_NAME);
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        System.out.println("------------------------------------");
+        Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
+        System.out.println("mat = " + mat.dump());
     }
 }
