cd3e5231491c4d5b57cdaab4eb9240f9af28f498
Student <davidayeluo@gmail.com>
2022-10-01 00:34:46 -0700

    Added video processing.

--- Makefile
+++ Makefile
@@ -4,3 +4,6 @@ run:
 	make -C ./src run
 clean:
 	make -C ./src clean
+	make -C ./output clean
+clean_media:
+	make -C ./media clean
--- src/Application.java
+++ src/Application.java
@@ -1,9 +1,9 @@
-import org.opencv.core.Core;
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
+import org.opencv.core.*;
+import org.opencv.videoio.VideoWriter;
+import org.opencv.videoio.VideoCapture;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
-import org.opencv.core.CvType;
+import org.opencv.videoio.Videoio;
 
 public class Application
 {
@@ -19,28 +19,86 @@ public class Application
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         System.out.println("------------------------------------");
 
-        String fileName = "0001.jpg";
+        String fileName = "0001.mp4";
         String imgPath = MEDIA_PATH + fileName;
+        String outImgPath = OUTPUT_FILES + fileName;
 
-        System.out.printf("Reading image from disk: %s.\n", imgPath);
-        Mat sourceImage = Imgcodecs.imread(imgPath);
+        System.out.printf("Reading image from disk: %s\n", imgPath);
 
+        int fourcc = VideoWriter.fourcc('a','v','c','1');
+//        int fourcc = VideoWriter.fourcc('h','2','6','4');
+        VideoCapture videoCapture = new VideoCapture(imgPath);
+
+        double fps = videoCapture.get(Videoio.CAP_PROP_FPS);
+        Size frameSize = new Size(
+                (int) videoCapture.get(Videoio.CAP_PROP_FRAME_WIDTH),
+                (int) videoCapture.get(Videoio.CAP_PROP_FRAME_HEIGHT));
+        VideoWriter videoWriter = new VideoWriter(outImgPath, fourcc, fps, frameSize,true);
+
+        if(!videoCapture.isOpened()) System.out.println("File didn't open");
+
+        // TODO: rename Image Matrix to Frame Matrix
+        Mat sourceImage = new Mat();
+        //        Mat sourceImage = Imgcodecs.imread(imgPath); // Used for images
+        Mat grayScaleImage = new Mat();
         Mat binaryImage = new Mat();
         Mat distanceTImage = new Mat();
+        Mat laplacianImage = new Mat();
 
-        System.out.println("Image processing: Grayscaling");
-        Imgproc.cvtColor(sourceImage, sourceImage,Imgproc.COLOR_BGR2GRAY);
+        for(int i = 0; i < 1; i++)
+        {
+            if(!videoCapture.isOpened()) continue;
+            videoCapture.read(sourceImage);
+            System.out.printf("Source Image: %s\n", sourceImage);
 
-        System.out.println("Image processing: Thresholding");
-        Imgproc.threshold(sourceImage, binaryImage, 100, 255, Imgproc.THRESH_BINARY);
-        binaryImage.convertTo(binaryImage, CvType.CV_8UC1);
+            System.out.println("Image processing: Grayscaling");
+            Imgproc.cvtColor(sourceImage, grayScaleImage,Imgproc.COLOR_BGR2GRAY);
+            System.out.printf("GrayScaled Image: %s\n", sourceImage);
 
-        System.out.println("Image processing: distance transform");
-        Imgproc.distanceTransform(binaryImage, distanceTImage, Imgproc.DIST_C, 3);
+            System.out.println("Image processing: Thresholding");
+//            Imgproc.threshold(grayScaleImage, binaryImage, 200, 255, Imgproc.THRESH_BINARY); // manual
+            Imgproc.threshold(grayScaleImage, binaryImage, 0, 255, Imgproc.THRESH_OTSU);
+//            binaryImage.convertTo(binaryImage, CvType.CV_8UC1);
+            System.out.printf("Binary Image: %s\n", binaryImage);
 
-        System.out.println("Finished processing");
+            System.out.println("Image processing: distance transform");
+            Imgproc.distanceTransform(binaryImage, distanceTImage, Imgproc.DIST_L2, 0);
+            System.out.printf("Image Transform: %s\n", distanceTImage);
 
-        System.out.printf("Writing image to disk: %s.\n", OUTPUT_FILES + fileName);
-        Imgcodecs.imwrite(OUTPUT_FILES + fileName, distanceTImage);
+
+            distanceTImage.convertTo(distanceTImage, CvType.CV_8UC1);
+            Imgproc.threshold(distanceTImage, laplacianImage, 0, 255, Imgproc.THRESH_OTSU);
+            System.out.println("Image processing: distance transform");
+            Imgproc.distanceTransform(laplacianImage, laplacianImage, Imgproc.DIST_L2, 0);
+            System.out.printf("Image Transform: %s\n", distanceTImage);
+            Imgproc.threshold(distanceTImage, laplacianImage, 0, 255, Imgproc.THRESH_OTSU);
+            Core.bitwise_not(laplacianImage, laplacianImage);
+            distanceTImage.convertTo(distanceTImage, CvType.CV_8UC1);
+//            distanceTImage.convertTo(distanceTImage, CvType.CV_8UC1);
+////            Core.bitwise_not(distanceTImage, distanceTImage);
+//            Imgproc.Laplacian(distanceTImage, laplacianImage, CvType.CV_8UC1, 3, 1, 0, Core.BORDER_DEFAULT);
+
+//            Imgproc.threshold(laplacianImage, laplacianImage, 0, 255, Imgproc.THRESH_OTSU);
+//            Imgproc.threshold(laplacianImage, laplacianImage, 0, 255, Imgproc.THRESH_BINARY); // manual
+
+            Scalar color = new Scalar(203,192, 255); // hot pink
+//            Scalar color = new Scalar(255,255,255); // white
+            sourceImage.setTo(color, laplacianImage);
+
+            System.out.println("Finished processing");
+
+            System.out.printf("Writing image to disk: %s.\n", OUTPUT_FILES + fileName);
+
+            Imgcodecs.imwrite(OUTPUT_FILES + "grayscale.jpg", grayScaleImage);
+            Imgcodecs.imwrite(OUTPUT_FILES + "binaryimage.jpg", binaryImage);
+            Imgcodecs.imwrite(OUTPUT_FILES + "transform.jpg", distanceTImage);
+            Imgcodecs.imwrite(OUTPUT_FILES + "laplacian.jpg", laplacianImage);
+            Imgcodecs.imwrite(OUTPUT_FILES + "result.jpg", sourceImage);
+
+            videoWriter.write(sourceImage);
+        }
+
+        videoCapture.release();
+        videoWriter.release();
     }
 }
