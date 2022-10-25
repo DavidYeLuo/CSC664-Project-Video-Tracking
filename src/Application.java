e64a53454b6b71c8e2759d7aa35043c57ca4cb0d
Student <davidayeluo@gmail.com>
2022-10-24 23:55:20 -0700

    Settings are now located at top of the code. Also, we are using Manhattan for dist transform. Also changed sobel kernel size.

--- src/Application.java
+++ src/Application.java
@@ -30,20 +30,37 @@ public class Application
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         System.out.println("------------------------------------");
 
+        // Settings
+        boolean DEBUG_MODE = true;
+        boolean DEBUG_USE_SIMPLE_IMAGE = false;
+        boolean DEBUG_SAVE_IMAGE = true;
+        boolean DEBUG_PRINT_IMAGE = false;
+        // Settings for image/video
         String fileName = "0001.mp4";
-//        String fileName = "0001.jpg";
+        int fourcc = VideoWriter.fourcc('a', 'v', 'c', '1');
+        int numOfFrames = 1; // Number of frames to process
+        // Object Tracking Settings
+        int numOfSections = 8; // arbitrary number for now
+        int radius = 1; // Box/circle around the tracking object
+        Scalar color = new Scalar(203,192, 255); // hot pink
+        // Sweet spot sobelMaxVal = 1, distMinVal = 1
+        int sobelMaxVal = 1; // closer to 0 -> More accurate on filter out non-peak
+        int distMinVal = 1; // closer to 0 -> More accurate on filtering background
+
+//        if(DEBUG_USE_SIMPLE_IMAGE) fileName = "0002.png";
+        if(DEBUG_USE_SIMPLE_IMAGE) fileName = "0001.jpg";
         String imgPath = MEDIA_PATH + fileName;
         String outImgPath = OUTPUT_FILES + fileName;
-
         System.out.printf("Reading image from disk: %s\n", imgPath);
-
-        boolean DEBUG_MODE = false;
-        boolean DEBUG_SAVE_IMAGE = false;
-        boolean DEBUG_PRINT_IMAGE = false;
-
-        int         fourcc       = VideoWriter.fourcc('a', 'v', 'c', '1');
-        FileManager fileManager = new VideoManager(imgPath, outImgPath, fourcc); // Comment for img
-//        FileManager fileManager = new ImageManager(imgPath, outImgPath); // Uncomment for img
+        FileManager fileManager;
+        if(DEBUG_USE_SIMPLE_IMAGE)
+        {
+            fileManager = new ImageManager(imgPath, outImgPath);
+        }
+        else
+        {
+            fileManager = new VideoManager(imgPath, outImgPath, fourcc);
+        }
 
         Mat sourceFrame = new Mat();
         Mat grayScaleFrame = new Mat();
@@ -54,15 +71,19 @@ public class Application
 //        Mat edgeY = new Mat();
         Mat sobelX = new Mat();
         Mat sobelY = new Mat();
+//        Mat firstSobel = new Mat();
         Mat finalSobel = new Mat();
 
         // Create a kernel that we will use to sharpen our image
 //        Mat kernel = Mat.ones(3, 3, CvType.CV_8U);
 
-        int numOfSections = 8; // arbitrary number for now
         List<Mat> sections = new ArrayList<>();
 
-        int numOfFrames = 1;
+        // Cache
+        Point topLeft; // Top left corner of the box
+        Point bottomRight; // Bottom right corner of the box
+
+        if(DEBUG_USE_SIMPLE_IMAGE) numOfFrames++;
         for(int i = 0; i < numOfFrames; i++)
         {
             if(fileManager.read(sourceFrame) == false) break;
@@ -71,6 +92,7 @@ public class Application
              * We will apply the otsu algorithm on each segment here and apply otsu algorithm here.
              */
             Imgproc.cvtColor(sourceFrame, grayScaleFrame,Imgproc.COLOR_BGR2GRAY);
+            Core.normalize(grayScaleFrame, grayScaleFrame, 0, 255, Core.NORM_MINMAX);
 
             // Cool but not sure if we need to use edge detection
 //            Imgproc.Sobel(grayScaleFrame, edgeX, CvType.CV_64F, 1, 0, 3, 1, 0, Core.BORDER_DEFAULT );
@@ -93,45 +115,29 @@ public class Application
 //                                      Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C, Imgproc.THRESH_BINARY_INV,
 //                                      7, 8);
 
-            Imgproc.distanceTransform(binaryFrame, distanceTFrame, Imgproc.DIST_L2, 3);
+            Imgproc.distanceTransform(binaryFrame, distanceTFrame, Imgproc.DIST_L1, 5);
 
-            Core.normalize(distanceTFrame, distanceTFrame, 0.0, 1.0, Core.NORM_MINMAX);
-            Core.multiply(distanceTFrame, new Scalar(255), distanceTFrame);
-
-
-            // Needs to convert to 64 bit image
-            // This way it will capture negative slopes
-            Imgproc.Sobel(distanceTFrame, sobelX, CvType.CV_64F, 1, 0, 3, 1, 0, Core.BORDER_DEFAULT );
-            Imgproc.Sobel(distanceTFrame, sobelY, CvType.CV_64F, 0, 1, 3, 1, 0, Core.BORDER_DEFAULT );
+//            Core.normalize(distanceTFrame, distanceTFrame, 0, 255, Core.NORM_MINMAX);
+//            Core.multiply(distanceTFrame, new Scalar(255), distanceTFrame);
 
 
+//         Calculates the first the change in pixel intensity of the distance transform
+//         Needs to convert to 64 bit image
+//         This way it will capture negative slopes
+            Imgproc.Sobel(distanceTFrame, sobelX, CvType.CV_64F, 1, 0, 0, 1, 0, Core.BORDER_DEFAULT );
+            Imgproc.Sobel(distanceTFrame, sobelY, CvType.CV_64F, 0, 1, 0, 1, 0, Core.BORDER_DEFAULT );
             Core.convertScaleAbs(sobelX, sobelX);
             Core.convertScaleAbs(sobelY, sobelY);
-
-
-            Core.addWeighted(sobelX, 0.5, sobelY, 0.5, 0, finalSobel);
-            Core.normalize(finalSobel, finalSobel, 0.0, 1.0, Core.NORM_MINMAX);
-            Core.multiply(finalSobel, new Scalar(255), finalSobel);
-
-            int radius = 3;
-            // 0 < sobelPixel < sobelMaxVal
-            // distMinVal < distanceTFrame < 255
-            // Sweet spot sobelMaxVal = 3, distMinVal = 11
-            int sobelMaxVal = 3; // Lower value -> More accurate but fewer finds
-            int distMinVal = 11; // Higher value -> More accurate but fewer finds
-            // Cache
-            Point topLeft;
-            Point bottomRight;
-            Scalar color = new Scalar(203,192, 255); // hot pink
+//            Core.addWeighted(sobelX, 0.5, sobelY, 0.5, 0, finalSobel);
+            Core.bitwise_or(sobelX, sobelY, finalSobel);
             for(int row = 0; row < finalSobel.rows(); row++)
             {
                 for(int col = 0; col < finalSobel.cols(); col++)
                 {
-                    // if sobel at (row,col) is 0
-                    // and distanceTFrame at (row,col)
-                    if(distanceTFrame.get(row, col)[0] <  distMinVal) continue;
-                    if(finalSobel.get(row,col)[0] > sobelMaxVal) continue; // Filters out
-
+                    // if sobel at (row,col) is < sobelMaxVal
+                    // and distanceTFrame at (row,col) > distMinVal
+                    if(distanceTFrame.get(row, col)[0] < distMinVal) continue; // Closer to 0 means likely a background
+                    if(finalSobel.get(row,col)[0] > sobelMaxVal) continue; // 0 Is the peak
                     topLeft = new Point(col-radius, row-radius);
                     bottomRight = new Point(col+radius, row+radius);
                     Imgproc.rectangle(sourceFrame, topLeft, bottomRight, color);
@@ -158,6 +164,8 @@ public class Application
             createTextImageFile("grayscale.txt", grayScaleFrame);
             createTextImageFile("binaryimage.txt", binaryFrame);
             createTextImageFile("transform.txt", distanceTFrame);
+            createTextImageFile("sobelX.txt", sobelX);
+            createTextImageFile("sobelY.txt", sobelY);
             createTextImageFile("sobel.txt", finalSobel);
             createTextImageFile("result.txt", sourceFrame);
         }
@@ -185,8 +193,10 @@ public class Application
             {
                 for(int col = 0; col < img.cols(); col++)
                 {
-                    stringBuilder.append(String.format("%03d", (int)(img.get(row,col)[0])));
+                    stringBuilder.append(String.format("%3d", (int)(img.get(row,col)[0])));
+//                    stringBuilder.append(String.format("(%d,%d)", col, (int)(img.get(row,col)[0])));
                     stringBuilder.append(" ");
+//                    stringBuilder.append(",");
                 }
                 stringBuilder.append("\n");
                 fileWriter.write(stringBuilder.toString());
