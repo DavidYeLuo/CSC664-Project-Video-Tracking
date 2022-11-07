0557c89626bb0de0b0ceb5015403ba4b93079cfb
chrisf725 <chfrancisco725@yahoo.com>
2022-11-06 16:25:59 -0800

    Refactored image splitting into a method

--- src/Application.java
+++ src/Application.java
@@ -13,6 +13,38 @@ public class Application
 {
     public static final String MEDIA_PATH = "../media/";
     public static final String OUTPUT_FILES = "../output/";
+    public static void ImageSplitAndMerge(int numOfSections, Mat sourceFrame) {
+        List<Mat> sections = new ArrayList<>();
+
+        int rowBlock = sourceFrame.rows() / numOfSections;
+        int colBlock = sourceFrame.cols() / numOfSections;
+
+        for(int col = 0; col < numOfSections; col++) {
+            for (int sect = 0; sect < numOfSections; sect++) {
+                sections.add(sourceFrame.submat(rowBlock*(sect), rowBlock*(1+sect), colBlock*(col), colBlock*(1+col)));
+
+                // Image processing
+                int index = sect + numOfSections * col;
+                Imgproc.cvtColor(sections.get(index), sections.get(index), Imgproc.COLOR_BGR2GRAY);
+                Imgproc.threshold(sections.get(index), sections.get(index), 0, 255, Imgproc.THRESH_OTSU);
+            }
+        }
+
+        ArrayList<Mat> mats = new ArrayList<>();
+        for(int index = 0; index < numOfSections; index++) {
+            mats.add(new Mat());
+        }
+
+        // Row merge
+        int temp2 = 0;
+        for (int temp = 0; temp < numOfSections * numOfSections; temp += numOfSections) {
+            Core.vconcat(sections.subList(temp, numOfSections + temp), mats.get(temp2));
+            temp2++;
+        }
+        // Column merge
+        Core.hconcat(mats, sourceFrame);
+    }
+
 
     public static void main(String[]args)
     {
@@ -31,7 +63,6 @@ public class Application
         System.out.printf("Reading image from disk: %s\n", imgPath);
 
         int fourcc = VideoWriter.fourcc('a','v','c','1');
-//        int fourcc = VideoWriter.fourcc('h','2','6','4');
         VideoCapture videoCapture = new VideoCapture(imgPath);
 
         double fps = videoCapture.get(Videoio.CAP_PROP_FPS);
@@ -51,54 +82,16 @@ public class Application
         Mat sobelY = new Mat();
         Mat finalSobel = new Mat();
 
-        int numOfSections = 8; // arbitrary number for now
-        List<Mat> sections = new ArrayList<>();
-
         int numOfFrames = 1;
         for(int i = 0; i < numOfFrames; i++)
         {
             if(!videoCapture.isOpened()) continue;
             videoCapture.read(sourceFrame);
 
-            /**
-             * TODO: Need to convert image splitting, processing, and merging into a method
-             */
-            int rowBlock = sourceFrame.rows() / numOfSections;
-            int colBlock = sourceFrame.cols() / numOfSections;
+            ImageSplitAndMerge(15, sourceFrame);
 
-            /**
-             * We will apply the otsu algorithm on each segment here and apply otsu algorithm here.
-             */
-            for(int col = 0; col < 8; col++)
-            {
-                for (int sect = 0; sect < numOfSections; sect++) {
-                        sections.add(sourceFrame.submat(rowBlock*(0+sect), rowBlock*(1+sect), colBlock*(0+col), colBlock*(1+col)));
-
-                        int index= sect + numOfSections * col;
-//                        System.out.println(index);
-                        Imgproc.cvtColor(sections.get(index), sections.get(index), Imgproc.COLOR_BGR2GRAY);
-                        Imgproc.threshold(sections.get(index), sections.get(index), 0, 255, Imgproc.THRESH_OTSU);
-                }
-
-            }
-
-
-            ArrayList<Mat> mats = new ArrayList<>();
-            for(int index = 0; index < 8; index++)
-            {
-                mats.add(new Mat());
-            }
-
-            int temp2 = 0;
-            for (int temp = 0; temp < 64; temp += 8) {
-                Core.vconcat(sections.subList(0 + temp, 8 + temp), mats.get(temp2));
-                temp2++;
-            }
-            Core.hconcat(mats, sourceFrame); // This merges images vertically
             sourceFrame.copyTo(grayScaleFrame);
 
-
-            /** Image processing */
 //            Imgproc.cvtColor(sourceFrame, grayScaleFrame,Imgproc.COLOR_BGR2GRAY);
 //
             Imgproc.threshold(grayScaleFrame, binaryFrame, 0, 255, Imgproc.THRESH_OTSU);
